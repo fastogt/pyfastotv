68a02ab33501fa0fcd38d19d279846c087bfc11c
topilski <atopilski@gmail.com>
2020-04-24 03:43:07 -0400

    Tags

--- stream/forms.py
+++ stream/forms.py
@@ -10,6 +10,48 @@ from wtforms.validators import InputRequired, Length, NumberRange, Optional
 from app.common.common_forms import SizeForm, LogoForm, RationalForm, RSVGLogoForm, OutputUrlForm, InputUrlForm
 
 
+class TagListField(StringField):
+    """Stringfield for a list of separated tags"""
+
+    def __init__(self, label='', validators=None, remove_duplicates=True, to_lowercase=False, separator=' ', **kwargs):
+        """
+        Construct a new field.
+        :param label: The label of the field.
+        :param validators: A sequence of validators to call when validate is called.
+        :param remove_duplicates: Remove duplicates in a case insensitive manner.
+        :param to_lowercase: Cast all values to lowercase.
+        :param separator: The separator that splits the individual tags.
+        """
+        super(TagListField, self).__init__(label, validators, **kwargs)
+        self.remove_duplicates = remove_duplicates
+        self.to_lowercase = to_lowercase
+        self.separator = separator
+        self.data = []
+
+    def _value(self):
+        if self.data:
+            return u', '.join(self.data)
+        else:
+            return u''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(self.separator)]
+            if self.remove_duplicates:
+                self.data = list(self._remove_duplicates(self.data))
+            if self.to_lowercase:
+                self.data = [x.lower() for x in self.data]
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner"""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
+
+
 class OptionalFormField(FormField):
     def process(self, formdata, *args, **kwargs):
         self._formdata = formdata
@@ -31,7 +73,8 @@ class IStreamForm(FlaskForm):
     tvg_logo = StringField('Icon:',
                            validators=[Optional(),
                                        Length(min=constants.MIN_URI_LENGTH, max=constants.MAX_URI_LENGTH)])
-    group = StringField('Group:', validators=[])
+    group = TagListField('Group:', separator=',',
+                         validators=[Length(max=8, message='You can only use up to 8 groups.')])
     price = FloatField('Price:',
                        validators=[InputRequired(), NumberRange(constants.MIN_PRICE, constants.MAX_PRICE)])
     visible = BooleanField('Visible for clients:', validators=[])
@@ -51,6 +94,7 @@ class IStreamForm(FlaskForm):
         entry.tvg_name = self.tvg_name.data
         entry.tvg_logo = self.tvg_logo.data
         entry.group = self.group.data
+
         entry.price = self.price.data
         entry.visible = self.visible.data
         entry.iarc = self.iarc.data
