ec458edf24f9d04a2574bcb91ec041d14f41a099
topilski <atopilski@gmail.com>
2020-07-11 08:46:05 -0400

    Review

--- subscriber/forms.py
+++ subscriber/forms.py
@@ -1,14 +1,13 @@
 import pyfastocloud_models.constants as constants
 from flask_wtf import FlaskForm
+from pyfastocloud_models.subscriber.entry import Subscriber
 from wtforms.fields import StringField, PasswordField, SubmitField, SelectField, IntegerField, DateTimeField
 from wtforms.validators import InputRequired, Length, Email, NumberRange
 
-from app.home.entry import SubscriberUser
-
 
 class SignUpForm(FlaskForm):
-    AVAILABLE_STATUSES = [(SubscriberUser.Status.NOT_ACTIVE, 'Not active'), (SubscriberUser.Status.ACTIVE, 'Active'),
-                          (SubscriberUser.Status.DELETED, 'Deleted')]
+    AVAILABLE_STATUSES = [(Subscriber.Status.NOT_ACTIVE, 'Not active'), (Subscriber.Status.ACTIVE, 'Active'),
+                          (Subscriber.Status.DELETED, 'Deleted')]
 
     email = StringField('Email:',
                         validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
@@ -19,9 +18,9 @@ class SignUpForm(FlaskForm):
                           choices=constants.AVAILABLE_COUNTRIES)
     language = SelectField('Language:', coerce=str, default=constants.DEFAULT_LOCALE,
                            choices=constants.AVAILABLE_LOCALES_PAIRS)
-    status = SelectField('Status:', coerce=SubscriberUser.Status.coerce, validators=[InputRequired()],
+    status = SelectField('Status:', coerce=Subscriber.Status.coerce, validators=[InputRequired()],
                          choices=AVAILABLE_STATUSES)
-    exp_date = DateTimeField(default=SubscriberUser.MAX_DATE)
+    exp_date = DateTimeField(default=Subscriber.MAX_DATE)
     max_devices_count = IntegerField('Max devices count:', default=constants.DEFAULT_DEVICES_COUNT,
                                      validators=[NumberRange(1, 100)])
     apply = SubmitField('Sign Up')
@@ -32,14 +31,14 @@ class SignUpForm(FlaskForm):
         else:
             self.password.validators = []
 
-    def make_entry(self) -> SubscriberUser:
-        return self.update_entry(SubscriberUser())
+    def make_entry(self) -> Subscriber:
+        return self.update_entry(Subscriber())
 
-    def update_entry(self, subscriber: SubscriberUser) -> SubscriberUser:
+    def update_entry(self, subscriber: Subscriber) -> Subscriber:
         subscriber.email = self.email.data.lower()
         subscriber.first_name = self.first_name.data
         subscriber.last_name = self.last_name.data
-        subscriber.password = SubscriberUser.generate_password_hash(self.password.data)
+        subscriber.password = Subscriber.generate_password_hash(self.password.data)
         subscriber.country = self.country.data
         subscriber.language = self.language.data
         subscriber.status = self.status.data
